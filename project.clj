3f356e622d8dc0ad362f32e8e2072c925ca643b4
Robert M. Avram <robert.avram@rewe-digital.com>
2018-02-23 12:22:16 +0100

    Upgrade to clojure 1.8

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject clojure-lanterna "0.10.0"
+(defproject clojure-lanterna "0.11.0"
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://multimud.github.io/clojure-lanterna/"
   :license {:name "LGPL"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.googlecode.lanterna/lanterna "3.0.0"]]
   :java-source-paths ["./java"]
-  ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
   :repositories {"releases" {:url "https://clojars.org/repo"
                              :username :env
                              :password :env
